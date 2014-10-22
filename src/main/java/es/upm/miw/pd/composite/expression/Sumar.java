5c36c5376027b37f9a6779bbb4ca0e86f80746cf
ajaragoneses <aj.aragoneses@gmail.com>
2014-10-22 20:39:51 +0200

    [composite.expression] Implementada la clase Sumar

--- src/main/java/es/upm/miw/pd/composite/expression/Sumar.java
+++ src/main/java/es/upm/miw/pd/composite/expression/Sumar.java
@@ -1,5 +1,16 @@
 package es.upm.miw.pd.composite.expression;
 
-public class Sumar {
+public class Sumar extends ExpresionBinaria {
 
+
+	public Sumar(Expresion ex_1, Expresion ex_2){
+		this.operando_1 = ex_1;
+		this.operando_2 = ex_2;
+		this.simbolo_operacion = "+";
+	}
+	
+	@Override
+	public int operar() {
+		return this.operando_1.operar() + this.operando_2.operar();
+	}
 }
