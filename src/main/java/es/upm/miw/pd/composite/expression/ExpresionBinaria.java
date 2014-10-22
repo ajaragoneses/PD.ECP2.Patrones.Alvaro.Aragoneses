c0a7184f4d6fc18ce6259470cf82f406f013c6f4
ajaragoneses <aj.aragoneses@gmail.com>
2014-10-22 20:34:00 +0200

    [composite.expression] Sobreescrito el método toString() en la clase ExpresionBinaria

--- src/main/java/es/upm/miw/pd/composite/expression/ExpresionBinaria.java
+++ src/main/java/es/upm/miw/pd/composite/expression/ExpresionBinaria.java
@@ -5,5 +5,10 @@ public abstract class ExpresionBinaria extends Expresion {
 	protected Expresion operando_1;
 	protected Expresion operando_2;
 	protected String simbolo_operacion;
+	
+	@Override
+	public String toString(){
+		return "("+this.operando_1.toString()+this.simbolo_operacion+this.operando_2.toString()+")";
+	}
 
 }
