1762f99f4e2aaf58bb9e887972f6009af80b121b
ajaragoneses <aj.aragoneses@gmail.com>
2014-10-22 20:44:13 +0200

    [composite.expression] Implementada la clase Numero

--- src/main/java/es/upm/miw/pd/composite/expression/Numero.java
+++ src/main/java/es/upm/miw/pd/composite/expression/Numero.java
@@ -1,5 +1,19 @@
 package es.upm.miw.pd.composite.expression;
 
-public class Numero {
+public class Numero extends Expresion{
+	private int numero;
+	
+	public Numero(int num){
+		this.numero = num;
+	}
+	
+	@Override
+	public int operar() {
+		return this.numero;
+	}
 
+	@Override
+	public String toString(){
+		return Integer.toString(this.numero);
+	}
 }
