5b88048df6f618bea33f293394a213e2512d9b8e
ajaragoneses <aj.aragoneses@gmail.com>
2014-10-22 20:29:08 +0200

    [component.expression] Creadas las clases ExpresionBinaria (parte comun de las operaciones) e implementada la clase Dividir

--- src/main/java/es/upm/miw/pd/composite/expression/Expresion.java
+++ src/main/java/es/upm/miw/pd/composite/expression/Expresion.java
@@ -1,6 +1,6 @@
 package es.upm.miw.pd.composite.expression;
 
-public abstract class Expresion {
+public abstract class Expresion{
 
 	public abstract int operar();
 
---
+++ src/main/java/es/upm/miw/pd/composite/expression/ExpresionBinaria.java
@@ -0,0 +1,9 @@
+package es.upm.miw.pd.composite.expression;
+
+public abstract class ExpresionBinaria extends Expresion {
+
+	protected Expresion operando_1;
+	protected Expresion operando_2;
+	protected String simbolo_operacion;
+
+}
